6cd4dc5206a3ae4b8addae2dfa4d26b5fcc2001b
Anton Kalashnikov <tohasan@yandex.ru>
2021-11-18 22:27:37 +0300

    common/combinator: merged combinator into generators

--- src/number-generator/generator/generator.spec.ts
+++ src/number-generator/generator/generator.spec.ts
@@ -9,28 +9,193 @@ describe('Generator', () => {
 
     describe('#generate', () => {
 
-        it('should generate all the variants of the car numbers by given facets', () => {
-            const facets = [['A', 'B'], ['0', '1'], ['4', '8'], [' '], ['78', '79']];
+        it('should generate only different options in each cell', () => {
+            const facets = [['A', 'B'], ['4', '8'], [' '], ['78', '79']];
 
-            const carNumbers = generator.generate(facets);
+            const combinations = generator.generate(facets);
 
-            expect(carNumbers).toEqual([
-                'A04 78',
-                'A04 79',
-                'A08 78',
-                'A08 79',
-                'A14 78',
-                'A14 79',
-                'A18 78',
-                'A18 79',
-                'B04 78',
-                'B04 79',
-                'B08 78',
-                'B08 79',
-                'B14 78',
-                'B14 79',
-                'B18 78',
-                'B18 79'
+            expect(combinations).toEqual([
+                'A4 78',
+                'B8 79'
+            ]);
+        });
+
+        it('should generate different options by a maximum cell', () => {
+            const facets = [['A', 'B'], ['1', '2', '3', '4', '5']];
+
+            const combinations = generator.generate(facets);
+
+            expect(combinations).toEqual([
+                'A1',
+                'B2',
+                'A3',
+                'B4',
+                'A5'
+            ]);
+        });
+
+        it('should generate all different combinations of letters following each other', () => {
+            const facets = [['A', 'B'], ['C', 'D'], ['E', 'F'], ['1', '2', '3', '4', '5'], ['M', 'N']];
+
+            const combinations = generator.generate(facets);
+
+            expect(combinations).toEqual([
+                'ACE1M',
+                'ACF2N',
+                'ADE3M',
+                'ADF4N',
+                'BCE5M',
+                'BCF1N',
+                'BDE2M',
+                'BDF3N'
+            ]);
+        });
+
+        it('should generate only different numeric combinations', () => {
+            const facets = [['A', 'B'], ['0', '2', '3'], ['4', '5']];
+
+            const combinations = generator.generate(facets);
+
+            expect(combinations).toEqual([
+                'A04',
+                'B05',
+                'A24',
+                'B35'
+            ]);
+        });
+
+        it('should generate special numeric combinations for 10-19', () => {
+            const facets = [['A', 'B'], ['1', '2'], ['0', '1', '5'], ['4', '8']];
+
+            const combinations = generator.generate(facets);
+
+            expect(combinations).toEqual([
+                'A104',
+                'B108',
+                'A114',
+                'B118',
+                'A154',
+                'B208'
+            ]);
+        });
+
+        it('should generate special numeric combinations starting from 0', () => {
+            const facets = [['A', 'B'], ['0', '2'], ['0', '3'], ['4', '8']];
+
+            const combinations = generator.generate(facets);
+
+            expect(combinations).toEqual([
+                'A004',
+                'B008',
+                'A034',
+                'B208'
+            ]);
+        });
+
+        it('should consider numeric combinations 20, 30, ... as others (nothing special)', () => {
+            const facets = [['A', 'B'], ['2', '3', '4'], ['0', '4', '8']];
+
+            const combinations = generator.generate(facets);
+
+            expect(combinations).toEqual([
+                'A20',
+                'B24',
+                'A28',
+                'B30',
+                'A44'
+            ]);
+        });
+
+        it('should consider numeric combinations for 100, 200, ... as others (nothing special)', () => {
+            const facets = [['A', 'B'], ['1', '2', '3'], ['0', '8'], ['0', '9']];
+
+            const combinations = generator.generate(facets);
+
+            expect(combinations).toEqual([
+                'A100',
+                'B109',
+                'A180',
+                'B209',
+                'A380'
+            ]);
+        });
+
+        it('should generate pairs of the same value for numbers', () => {
+            const facets = [['A', 'B'], ['1', '2', '3'], ['1', '2', '8'], ['3', '9']];
+
+            const combinations = generator.generate(facets);
+
+            expect(combinations).toEqual([
+                'A113',
+                'B119',
+                'A123',
+                'B189',
+                'A213',
+                'B223',
+                'A329'
+            ]);
+        });
+
+        it('should generate pairs of the same value for letters', () => {
+            const facets = [['A', 'B', 'C'], ['A', 'C', 'D'], ['2', '8'], ['A', 'B']];
+
+            const combinations = generator.generate(facets);
+
+            expect(combinations).toEqual([
+                'AA2A',
+                'AC8B',
+                'AD2A',
+                'BA8B',
+                'BC2A',
+                'BD8B',
+                'CA2A',
+                'CC8B',
+                'CD2A'
+            ]);
+        });
+
+        it('should generate triples of the same value for numbers', () => {
+            const facets = [['A', 'B'], ['2', '3'], ['2', '3'], ['2', '3', '4']];
+
+            const combinations = generator.generate(facets);
+
+            expect(combinations).toEqual([
+                'A222',
+                'B223',
+                'A224',
+                'B232',
+                'A233',
+                'B323',
+                'A333'
+            ]);
+        });
+
+        it('should generate triples of the same value for letters', () => {
+            const facets = [['A', 'B'], ['A', 'B'], ['A', 'B'], ['3', '9']];
+
+            const combinations = generator.generate(facets);
+
+            expect(combinations).toEqual([
+                'AAA3',
+                'AAB9',
+                'ABA3',
+                'ABB9',
+                'BAA3',
+                'BAB9',
+                'BBA3',
+                'BBB9'
+            ]);
+        });
+
+        it('should not generate zero numbers', () => {
+            const facets = [['A', 'B'], ['0', '1'], ['0', '2']];
+
+            const combinations = generator.generate(facets);
+
+            expect(combinations).toEqual([
+                'A02',
+                'B10',
+                'A12'
             ]);
         });
     });
--- src/voiceover-generator/generator/generator.ts
+++ src/voiceover-generator/generator/generator.ts
@@ -1,21 +1,30 @@
 import { Voiceover, VoiceoverKey } from '../entities/voiceover';
 import { VoiceoverDictionary } from '../entities/voiceover-dictionary';
 import { VoiceoverFacetSet } from '../entities/voiceover-facet';
-import { Combinator } from '../../common/combinator/combinator';
+import { Facet } from '../../common/entities/facet';
+import { DisjointCombination } from '../../common/entities/disjoint-combination';
 
 export class Generator {
-    private combinator = new Combinator();
 
     generate(keySets: VoiceoverKey[][], dictionary: VoiceoverDictionary): Voiceover[] {
         return keySets.flatMap(keySet => {
             const name = keySet.join('');
             const facets = this.getFacetByKeys(keySet, dictionary);
-            return this.combinator.mix(facets)
+            return this.cartesianProduct(facets)
                 .map(set => ({ name, options: [set.join(' ')] }));
         });
     }
 
     private getFacetByKeys(keySet: VoiceoverKey[], dictionary: VoiceoverDictionary): VoiceoverFacetSet {
         return keySet.map(key => dictionary[key]);
+    }
+
+    private cartesianProduct(facets: Facet[]): DisjointCombination[] {
+        return facets.reduce<DisjointCombination[]>(
+            (accSets, facet) => {
+                return accSets.flatMap(accSet => facet.map(value => [...accSet, value]));
+            },
+            [[]]
+        );
     }
 }
