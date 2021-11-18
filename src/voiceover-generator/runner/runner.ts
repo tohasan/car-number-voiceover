7d47f239c482be3237d3e267229c84c338b393d2
Anton Kalashnikov <tohasan@yandex.ru>
2021-11-19 00:29:18 +0300

    voiceover-generator: added logging results

--- src/voiceover-generator/runner/runner.ts
+++ src/voiceover-generator/runner/runner.ts
@@ -4,6 +4,7 @@ import { DictionaryReader } from '../dictionary-reader/dictionary-reader';
 import { CarNumbersReader } from '../car-numbers-reader/car-numbers-reader';
 import { GeneratorCli } from '../generator-cli/generator-cli';
 import { FacetsGenerator } from '../facets-generator/facets-generator';
+import { Logger } from '../../common/utils/logger/logger';
 
 export class Runner {
 
@@ -13,13 +14,21 @@ export class Runner {
     private readonly facetsGenerator = new FacetsGenerator();
     private readonly generator = new Generator();
     private readonly exporter = new CsvExporter();
+    private readonly logger = new Logger();
 
     run(args: string[]): void {
+        const startTime = Date.now();
+
         const { input, dictionary, output } = this.cli.parse(args);
         const carNumbers = this.carNumbersReader.read(input);
         const dict = this.dictionaryReader.read(dictionary);
         const keySets = this.facetsGenerator.generate(carNumbers, Object.keys(dict));
         const voiceovers = this.generator.generate(keySets, dict);
         this.exporter.export(output, voiceovers);
+
+        this.logger.log(`Number of input car numbers: ${carNumbers.length}`);
+        this.logger.log(`Number of generated voiceovers: ${voiceovers.length}`);
+        this.logger.log(`You can find the result in the file: ${output}`);
+        this.logger.log(`Time spent: ${Date.now() - startTime} ms`);
     }
 }
